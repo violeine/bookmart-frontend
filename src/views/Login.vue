19aa35d435e46cdfe527a6c7d415835748c45322
violeine <haluudang@gmail.com>
2020-06-02 20:49:57 +0700

    add tailwind

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="#">
+    <label for="username">
+      username
+      <input type="text" name="username" id="username" />
+    </label>
+    <br />
+    <br />
+    <label for="password">
+      password
+      <input type="password" name="password" id="password" />
+    </label>
+  </form>
+</template>     
