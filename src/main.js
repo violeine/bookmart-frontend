e7c8a398201260816eb086d4d2e8c76dcd1f5ece
violeine <haluudang@gmail.com>
2020-06-02 20:20:37 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createProvider } from './vue-apollo'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  apolloProvider: createProvider(),
+  store,
+  render: h => h(App)
 }).$mount('#app')
